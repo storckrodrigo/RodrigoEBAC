45763e9412ef1e47552c89f0a1813f0389911c96
storckrodrigo <storck.rodrigo@gmail.com>
2026-05-18 18:14:30 -0300

    Opção de Saida
    inclusão no sistema a opção de saída

--- Cartorio2.c
+++ Cartorio2.c
@@ -124,6 +124,7 @@ int main()
     	printf("\t1 - Registrar nomes\n");
     	printf("\t2 - Consultar nomes\n");
     	printf("\t3 - Deletar nomes\n\n");
+    	printf("\t4 - sair do sistema\n\n");
     	printf("Opção: "); //final do menu
     	scanf("%d",&opcao); //armazenando escolha do usuário
 	
@@ -143,6 +144,11 @@ int main()
 		    case 3:
 	    	deletar();
 			break;
+			
+			case 4:
+				printf("Obrigado por ultilizar o sistema\n");
+				return 0;
+				break;
 			
 			
 			default:
